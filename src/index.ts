f6ce409903c0f309bd9146ee3c85a4dfdea3dea6
Rahul vanmali <rahul@nextbillion.ai>
2023-05-13 23:01:22 +0530

    fix: hls processing fixed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { dash } from "./convertors/dash";
+import { hls } from "./convertors/hls";
 import { preview_gif } from "./convertors/preview-gif";
 import { preview_thumbnail } from "./convertors/preview-thumbnail";
 
-// export { dash, preview_thumbnail, preview_gif };
+export { dash, preview_thumbnail, preview_gif,hls };
